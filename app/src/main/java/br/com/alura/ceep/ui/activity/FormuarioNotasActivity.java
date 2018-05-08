43903839b5c22d5bdfd3bd24afe6fe73a956525a
flavio.santana <9090flavio@gmail.com>
2018-05-07 21:57:12 -0300

    Adicionando o menu no formulário

--- app/src/main/java/br/com/alura/ceep/ui/activity/FormuarioNotasActivity.java
+++ app/src/main/java/br/com/alura/ceep/ui/activity/FormuarioNotasActivity.java
@@ -2,6 +2,7 @@ package br.com.alura.ceep.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import br.com.alura.ceep.R;
 
@@ -11,5 +12,11 @@ public class FormuarioNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formuario_notas);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_form_acoes, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
